1268fcddae909f9199e88c2fcb58c25c1ef236ef
Cristhian <valenciacristhian47@gmail.com>
2017-04-14 21:13:54 -0300

    Compose Element

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -13,9 +13,6 @@ define('app',["exports"], function (exports) {
 
   var App = exports.App = function App() {
     _classCallCheck(this, App);
-
-    this.message = "Hello Aurelia!";
-    this.image = "http://aurelia.io/images/main-logo.svg";
   };
 });
 define('environment',["exports"], function (exports) {
@@ -70,5 +67,41 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
-define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"bootstrap/css/bootstrap.css\"></require><div class=\"container-fluid\"><div class=\"h1\">${message}</div><div><img src.bind=\"image\" alt=\"Aurelia\"></div></div></template>"; });
+define('sponsors',["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var Sponsors = exports.Sponsors = function Sponsors() {
+        _classCallCheck(this, Sponsors);
+    };
+});
+define('events',["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var Events = exports.Events = function Events() {
+        _classCallCheck(this, Events);
+    };
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"bootstrap/css/bootstrap.css\"></require><nav class=\"navbar navbar-default\"><div class=\"container-fluid\"><div class=\"navbar-header\"><button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\"><span class=\"sr-only\">Toggle navigation</span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span></button> <a class=\"navbar-brand\" href=\"#\">Aurelia Fundamentals</a></div><div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\"><ul class=\"nav navbar-nav\"><li class=\"active\"><a href=\"#\">Events <span class=\"sr-only\">(current)</span></a></li><li><a href=\"#\">Discussion</a></li><li><a href=\"#\">Job Board</a></li></ul></div></div></nav><div class=\"container-fluid\"><div class=\"col-xs-10\"><compose view-model=\"events\"></compose></div><div class=\"col-xs-2\"><compose view-model=\"sponsors\"></compose></div></div></template>"; });
+define('text!sponsors.html', ['module'], function(module) { module.exports = "<template>Sponsors</template>"; });
+define('text!events.html', ['module'], function(module) { module.exports = "<template>Events</template>"; });
 //# sourceMappingURL=app-bundle.js.map
--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 export class App {
   constructor() {
-    this.message = "Hello Aurelia!";
-    this.image = "http://aurelia.io/images/main-logo.svg"
+    
   }
 }
